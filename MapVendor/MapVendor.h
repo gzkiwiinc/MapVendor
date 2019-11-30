296b5446eda51504617f69f49ae1b6b618719ab0
Kyle <lacklock@gmail.com>
2019-11-30 18:45:31 +0800

    add framework

---
+++ MapVendor/MapVendor.h
@@ -0,0 +1,19 @@
+//
+//  MapVendor.h
+//  MapVendor
+//
+//  Created by 卓同学 on 2019/11/29.
+//  Copyright © 2019 kiwi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapVendor.
+FOUNDATION_EXPORT double MapVendorVersionNumber;
+
+//! Project version string for MapVendor.
+FOUNDATION_EXPORT const unsigned char MapVendorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapVendor/PublicHeader.h>
+
+
